c0cb15ebca70ac6ace25a3d789e9289874e27f8c
GolfNorth <golf155@yandex.ru>
2021-08-04 00:05:12 +0500

    Adding command service

---
+++ Assets/Scripts/Assemblies/Game/Common/CommandService.cs
@@ -0,0 +1,80 @@
+﻿using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    /// <summary>
+    /// Сервис команд
+    /// </summary>
+    public sealed class CommandService
+    {
+        /// <summary>
+        /// Пространство сущностей
+        /// </summary>
+        private readonly EcsWorld _world;
+
+        /// <summary>
+        /// Сущность команд
+        /// </summary>
+        private readonly uint _entityId;
+
+        /// <summary>
+        /// Конструктор сервиса команд
+        /// </summary>
+        /// <param name="game">Геймплейный объект</param>
+        public CommandService(Game game)
+        {
+            _world = game.World;
+            _entityId = _world.CreateEntity();
+
+            _world.AddComponent<CommandComponent>(_entityId);
+        }
+
+        /// <summary>
+        /// Передвижение корабля
+        /// </summary>
+        public void Translate(float value)
+        {
+            ref CommandComponent commandComponent = ref GetComponent();
+
+            commandComponent.Translation = value;
+        }
+
+        /// <summary>
+        /// Поворот корабля
+        /// </summary>
+        public void Rotate(float value)
+        {
+            ref CommandComponent commandComponent = ref GetComponent();
+
+            commandComponent.Rotation = value;
+        }
+
+        /// <summary>
+        /// Выстрел корабля
+        /// </summary>
+        public void Fire(int value)
+        {
+            ref CommandComponent commandComponent = ref GetComponent();
+
+            commandComponent.Fire = value > 0;
+        }
+
+        /// <summary>
+        /// Альтернативный выстрел корабля
+        /// </summary>
+        public void AltFire(int value)
+        {
+            ref CommandComponent commandComponent = ref GetComponent();
+
+            commandComponent.AltFire = value > 0;
+        }
+
+        /// <summary>
+        /// Получить компонент команд
+        /// </summary>
+        private ref CommandComponent GetComponent()
+        {
+            return ref _world.GetComponent<CommandComponent>(_entityId);
+        }
+    }
+}
---
+++ Assets/Scripts/Assemblies/Game/Components/CommandComponent.cs
@@ -0,0 +1,28 @@
+﻿namespace NonUnity.Game
+{
+    /// <summary>
+    /// Компонент команд
+    /// </summary>
+    public struct CommandComponent
+    {
+        /// <summary>
+        /// Передвижение корабля
+        /// </summary>
+        public float Translation;
+
+        /// <summary>
+        /// Поворот корабля
+        /// </summary>
+        public float Rotation;
+
+        /// <summary>
+        /// Выстрел корабля
+        /// </summary>
+        public bool Fire;
+
+        /// <summary>
+        /// Альтернативный выстрел корабля
+        /// </summary>
+        public bool AltFire;
+    }
+}
