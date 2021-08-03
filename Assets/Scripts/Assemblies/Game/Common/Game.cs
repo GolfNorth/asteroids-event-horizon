c91620814c9a7cf4372846c19469cf4dab0f3548
GolfNorth <golf155@yandex.ru>
2021-08-04 01:30:38 +0500

    Adding game and game builder

---
+++ Assets/Scripts/Assemblies/Game/Common/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    /// <summary>
+    /// Класс геймплейного объекта
+    /// </summary>
+    public sealed class Game
+    {
+        /// <summary>
+        /// Конфигурация игры
+        /// </summary>
+        internal readonly GameSettings Settings;
+
+        /// <summary>
+        /// Рандомизатор
+        /// </summary>
+        internal Random Random { get; }
+
+        /// <summary>
+        /// Фабрика сущностей
+        /// </summary>
+        internal EntityFactory Factory { get; }
+
+        /// <summary>
+        /// Пространство сущностей
+        /// </summary>
+        public EcsWorld World { get; }
+
+        /// <summary>
+        /// Сервис команд
+        /// </summary>
+        public CommandService Command { get; }
+
+        /// <summary>
+        /// Конструктор объекта
+        /// </summary>
+        /// <param name="world">Пространство сущностей</param>
+        /// <param name="settings">Конфигурация игры</param>
+        /// <param name="viewFactory">Фабрика визуализаторов</param>
+        internal Game(EcsWorld world, GameSettings settings, IViewFactory viewFactory)
+        {
+            World = world;
+            Settings = settings;
+            Random = new Random();
+            Factory = new EntityFactory(this, viewFactory);
+            Command = new CommandService(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Assemblies/Game/Common/GameBuilder.cs
@@ -0,0 +1,134 @@
+﻿using System.Drawing;
+using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    /// <summary>
+    /// Класс формирования экземпляра геймплейного объекта
+    /// </summary>
+    public sealed class GameBuilder
+    {
+        /// <summary>
+        /// Игровые границы
+        /// </summary>
+        private readonly RectangleF _bounds;
+
+        /// <summary>
+        /// Фабрика визуализаторов
+        /// </summary>
+        private readonly IViewFactory _viewFactory;
+
+        /// <summary>
+        /// Пространство сущностей
+        /// </summary>
+        private EcsWorld _world;
+
+        /// <summary>
+        /// Конфигурация корабля
+        /// </summary>
+        private ShipSettings _shipSettings = new ShipSettings
+        {
+            AngularSpeed = ShipSettings.DefaultAngularSpeed,
+            LinearSpeed = ShipSettings.DefaultLinearSpeed,
+            StopSpeed = ShipSettings.DefaultStopSpeed,
+            Inertia = ShipSettings.DefaultInertia,
+            BulletSpeed = ShipSettings.DefaultBulletSpeed,
+            BulletFireRate = ShipSettings.DefaultBulletFireRate,
+            LaserFireRate = ShipSettings.DefaultLaserFireRate,
+            LaserFireDuration = ShipSettings.DefaultLaserFireDuration,
+            LaserMaxCharges = ShipSettings.DefaultLaserMaxCharges,
+            LaserCooldown = ShipSettings.DefaultLaserCooldown,
+            ShapeVertexes = ShipSettings.DefaultShapeVertexes
+        };
+
+        /// <summary>
+        /// Конфигурация атероидов
+        /// </summary>
+        private AsteroidSettings _asteroidSettings = new AsteroidSettings
+        {
+            SpawnDelay = AsteroidSettings.DefaultSpawnDelay,
+            SizeSettings = AsteroidSettings.DefaultSizeSettings
+        };
+
+        /// <summary>
+        /// Конфигурация НЛО
+        /// </summary>
+        private UfoSettings _ufoSettings = new UfoSettings
+        {
+            SpawnDelay = UfoSettings.DefaultSpawnDelay,
+            LinearSpeed = UfoSettings.DefaultLinearSpeed,
+            ShapeVertexes = UfoSettings.DefaultShapeVertexes
+        };
+
+        /// <summary>
+        /// Игровые границы
+        /// </summary>
+        /// <param name="bounds">Игровые границы</param>
+        /// <param name="viewFactory">Фабрика визуализаторов</param>
+        public GameBuilder(RectangleF bounds, IViewFactory viewFactory)
+        {
+            _bounds = bounds;
+            _viewFactory = viewFactory;
+        }
+
+        /// <summary>
+        /// Установить пространство сущностей
+        /// </summary>
+        /// <param name="world">Пространство сущностей</param>
+        public GameBuilder SetWorld(EcsWorld world)
+        {
+            _world = world;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Установить конфигурацию корабля
+        /// </summary>
+        /// <param name="shipSettings">Конфигурация корабля</param>
+        public GameBuilder SetShipSettings(in ShipSettings shipSettings)
+        {
+            _shipSettings = shipSettings;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Установить конфигурацию астероидов
+        /// </summary>
+        /// <param name="asteroidSettings">Конфигурация астероидов</param>
+        public GameBuilder SetAsteroidSettings(in AsteroidSettings asteroidSettings)
+        {
+            _asteroidSettings = asteroidSettings;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Установить конфигурацию НЛО
+        /// </summary>
+        /// <param name="ufoSettings">Конфигурация НЛО</param>
+        public GameBuilder SetAsteroidSettings(in UfoSettings ufoSettings)
+        {
+            _ufoSettings = ufoSettings;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Сформировать экземпляр геймплейного объекта
+        /// </summary>
+        public Game Build()
+        {
+            GameSettings gameSettings = new GameSettings
+            {
+                Bounds = _bounds,
+                Ship = _shipSettings,
+                Asteroid = _asteroidSettings,
+                Ufo = _ufoSettings
+            };
+
+            return new Game(_world ?? new EcsWorld(), gameSettings, _viewFactory);
+        }
+    }
+}
