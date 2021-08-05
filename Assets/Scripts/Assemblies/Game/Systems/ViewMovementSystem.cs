089f71b7bcf5937e19863d167d8732ae2e75ed58
GolfNorth <golf155@yandex.ru>
2021-08-06 02:34:01 +0500

    View movement system

---
+++ Assets/Scripts/Assemblies/Game/Systems/ViewMovementSystem.cs
@@ -0,0 +1,29 @@
+﻿using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    /// <summary>
+    /// Система перемещения визуализаторов
+    /// </summary>
+    public class ViewMovementSystem : GameSystem, IUpdateSystem
+    {
+        private readonly EcsFilter<TransformComponent, ViewComponent> _filter;
+
+        public ViewMovementSystem(Game game) : base(game)
+        {
+            _filter = new EcsFilter<TransformComponent, ViewComponent>(World);
+        }
+
+        public void Update(float dt)
+        {
+            foreach (uint entity in _filter.Entities)
+            {
+                ref TransformComponent transform = ref World.GetComponent<TransformComponent>(entity);
+                ref ViewComponent view = ref World.GetComponent<ViewComponent>(entity);
+
+                view.Value.Position = transform.Position;
+                view.Value.Rotation = transform.Rotation;
+            }
+        }
+    }
+}
