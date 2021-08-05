98050c537606a4f1770335e1807eec628f78dcc9
GolfNorth <golf155@yandex.ru>
2021-08-06 02:33:46 +0500

    Transform system

---
+++ Assets/Scripts/Assemblies/Game/Systems/TransformSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    /// <summary>
+    /// Система обновления трансформов
+    /// </summary>
+    public class TransformSystem : GameSystem, IUpdateSystem
+    {
+        private readonly EcsFilter<TransformComponent, MovementComponent> _filter;
+
+        public TransformSystem(Game game) : base(game)
+        {
+            _filter = new EcsFilter<TransformComponent, MovementComponent>(World);
+        }
+
+        public void Update(float dt)
+        {
+            foreach (uint entity in _filter.Entities)
+            {
+                ref TransformComponent transform = ref World.GetComponent<TransformComponent>(entity);
+                ref MovementComponent movement = ref World.GetComponent<MovementComponent>(entity);
+
+                transform.Rotation = (float) (Math.Atan2(movement.Direction.Y, movement.Direction.X) * (180 / Math.PI));
+                transform.Position += movement.Velocity * dt;
+            }
+        }
+    }
+}
