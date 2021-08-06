ade1616e89f2f7f3f6397eddc00a6e57ae0d4c65
GolfNorth <golf155@yandex.ru>
2021-08-07 01:23:52 +0500

    Ufo spawn system

---
+++ Assets/Scripts/Assemblies/Game/Systems/UfoSpawnSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    public class UfoSpawnSystem : AutoSpawnSystem
+    {
+        /// <summary>
+        /// Фильтр сущностей
+        /// </summary>
+        private readonly EcsFilter<UfoComponent> _filter;
+
+        public UfoSpawnSystem(Game game) : base(game, game.Settings.Ufo.SpawnDelay, game.Settings.Ufo.SpawnDelay)
+        {
+            _filter = new EcsFilter<UfoComponent>(World);
+        }
+
+        protected override void Restart()
+        {
+            base.Restart();
+
+            uint[] entities = _filter.Entities.ToArray();
+
+            foreach (uint entity in entities)
+            {
+                World.DestroyEntity(entity);
+            }
+        }
+
+        protected override void Spawn()
+        {
+            uint entity = Game.Factory.CreateUfo();
+
+            ref TransformComponent transform = ref World.GetComponent<TransformComponent>(entity);
+            transform.Position = GetRandomPosition(Game.Settings.Ufo.Offset);
+        }
+    }
+}
