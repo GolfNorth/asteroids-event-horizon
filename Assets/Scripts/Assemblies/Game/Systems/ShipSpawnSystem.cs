2290ef1bea36fa9e88b91085761f6c2ad8b54875
GolfNorth <golf155@yandex.ru>
2021-08-07 00:32:40 +0500

    Base spawn system

--- Assets/Scripts/Assemblies/Game/Systems/ShipSpawnSystem.cs
+++ Assets/Scripts/Assemblies/Game/Systems/ShipSpawnSystem.cs
@@ -1,27 +1,16 @@
-﻿using NonUnity.Ecs;
-
-namespace NonUnity.Game
+﻿namespace NonUnity.Game
 {
     /// <summary>
     /// Система респауна корабля
     /// </summary>
-    public sealed class ShipSpawnSystem : GameSystem, IUpdateSystem
+    public sealed class ShipSpawnSystem : SpawnSystem
     {
-        /// <summary>
-        /// Фильтр сущностей
-        /// </summary>
-        private readonly EcsFilter<RestartComponent> _filter;
-
         public ShipSpawnSystem(Game game) : base(game)
         {
-            _filter = new EcsFilter<RestartComponent>(World);
         }
 
-        public void Update(float dt)
+        protected override void Restart()
         {
-            if (_filter.Entities.Count == 0)
-                return;
-
             Game.Factory.CreateShip();
         }
     }
---
+++ Assets/Scripts/Assemblies/Game/Systems/SpawnSystem.cs
@@ -0,0 +1,33 @@
+﻿using NonUnity.Ecs;
+
+namespace NonUnity.Game
+{
+    /// <summary>
+    /// Система респауна
+    /// </summary>
+    public abstract class SpawnSystem : GameSystem, IUpdateSystem
+    {
+        /// <summary>
+        /// Фильтр рестарта
+        /// </summary>
+        private readonly EcsFilter<RestartComponent> _restartFilter;
+
+        protected SpawnSystem(Game game) : base(game)
+        {
+            _restartFilter = new EcsFilter<RestartComponent>(World);
+        }
+
+        public virtual void Update(float dt)
+        {
+            if (_restartFilter.Entities.Count > 0)
+            {
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// Рестарт игры
+        /// </summary>
+        protected abstract void Restart();
+    }
+}
